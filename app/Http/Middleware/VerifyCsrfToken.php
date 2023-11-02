597097c43b0cf1eb6cb2372696f284871d7da311
Luiz Felipe P Carvalho <kranildo@gmail.com>
2023-11-02 12:02:05 -0300

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'http://luizfpimentel-lv.eu1.alfasoft.pt/api/contacts',
     ];
 }
